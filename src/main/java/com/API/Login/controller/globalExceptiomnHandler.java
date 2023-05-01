247e60215c54927675fa9ad245b79ad492d0bcd3
santoshchaurasiya <santoshchaurasiya316@gmail.com>
2023-05-01 10:28:25 +0530

    this is my third push

--- src/main/java/com/API/Login/controller/globalExceptiomnHandler.java
+++ src/main/java/com/API/Login/controller/globalExceptiomnHandler.java
@@ -21,7 +21,7 @@ public class globalExceptiomnHandler{
 		String message = ex.getMessage();
 		ApiResponse apiResponse=new ApiResponse();
 		apiResponse.setError(404);
-		apiResponse.setMessage("Not Found!!");
+		apiResponse.setMessage("Not Found!!!");
 		
 		return message;
 	}
